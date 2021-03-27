dc2c2fdfa9c0ab91ac8b0cad61ffade690fdf44e
sonashubhamvats <sona.svats23@gmail.com>
2021-03-28 01:54:30 +0530

    location vendor

--- app/src/main/java/com/example/localvendorsupplychain/locationSearchActivity.java
+++ app/src/main/java/com/example/localvendorsupplychain/locationSearchActivity.java
@@ -16,9 +16,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class locationSearchActivity extends AppCompatActivity {
 
@@ -26,6 +34,8 @@ public class locationSearchActivity extends AppCompatActivity {
     LocationListener locationListener;
     Button searchWithName, searchWithLocation, seeOnMapButton;
     LatLng myLocation;
+    static LatLng[] allTheLocations;
+    ListView listOfVendors;
 
 
     @Override
@@ -53,7 +63,7 @@ public class locationSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_search);
-
+        listOfVendors=findViewById(R.id.listOfVendors);
         searchWithLocation = findViewById(R.id.searchWithLocationButton);
         searchWithName = findViewById(R.id.searchWithNameButton);
         seeOnMapButton = findViewById(R.id.seeOnMapButton);
@@ -76,14 +86,33 @@ public class locationSearchActivity extends AppCompatActivity {
           ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
-    void searchWithLocationButton()
+    void fillTheList()
     {
-        Intent intent=new Intent(this,locationActivity.class);
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("userinfo").child("vendors");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren())
+                {
+                    Vendor v=snapshot.getValue(Vendor.class);
+                    HashMap<String,Object> hashMap=v.AddDataToUserDataBase();
+                    Toast.makeText(locationSearchActivity.this, ""+hashMap.get("lat"), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        /*Intent intent=new Intent(this,locationActivity.class);
         intent.putExtra("Lat",myLocation.latitude);
         intent.putExtra("Long",myLocation.longitude);
-        startActivity(intent);
+        startActivity(intent);*/
     }
-
+    void searchWithLocationButton()
+    {
+    }
     void LocationServices() {
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
@@ -93,7 +122,7 @@ public class locationSearchActivity extends AppCompatActivity {
                 myLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 Toast.makeText(locationSearchActivity.this, ""+myLocation, Toast.LENGTH_SHORT).show();
                 locationManager.removeUpdates(locationListener);
-                seeOnMapButton.setVisibility(View.VISIBLE);
+                //seeOnMapButton.setVisibility(View.VISIBLE);
                 //call the function that can get the
             }
 
